8c13ceef6c3aa0f6d04c716a42f527c934daf2a9
Judy Hu <judy.hu9@gmail.com>
2018-12-12 01:36:56 -0500

    set up

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+var friendList = require('../data/friends.js');
+var path = require('path');
+
+//==============================================
+//ROUTING
+//==============================================
+
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friendList);
+	});
+
+	app.post('/api/friends', function(req, res){
+		friendList.push(req.body);
+	})
+};
